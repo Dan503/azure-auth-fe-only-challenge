6b7fd294856efa2f71355ac75ab16bf8a72a1dbd
Daniel Tonon <dan503.npm@gmail.com>
2024-08-21 11:07:34 +1000

    improved handling of Unauthorized Page

--- src/routes/UnauthorizedPage.tsx
+++ src/routes/UnauthorizedPage.tsx
@@ -1,10 +1,22 @@
 import { Link, useNavigate, useSearchParams } from 'react-router-dom'
 import { LoginButton } from '../components/LogInButton'
+import { useEffect } from 'react'
+import { useAuthContext } from '../components/AuthContext'
 
 export function UnauthorizedPage() {
+	const { account } = useAuthContext()
 	const [searchParams] = useSearchParams()
 	const redirectPath = searchParams.get('path')
 	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (account && redirectPath) {
+			navigate(redirectPath, {
+				replace: true,
+			})
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [account, redirectPath])
 
 	return (
 		<>
@@ -12,15 +24,7 @@ export function UnauthorizedPage() {
 			<p>You are not authorized to view that page</p>
 
 			<p>
-				<LoginButton
-					onLogin={() => {
-						if (redirectPath) {
-							navigate(redirectPath, {
-								replace: true,
-							})
-						}
-					}}
-				/>
+				<LoginButton />
 			</p>
 			<p>
 				<Link to="/">Back to home</Link>
