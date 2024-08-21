98561d9b539a5d3c40fedea1441ccdce93b22bda
Daniel Tonon <dan503.npm@gmail.com>
2024-08-21 11:32:19 +1000

    hard-coding clientID

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,11 @@ const baseUri = `${window.location.origin}/azure-auth-fe-only-challenge/`
 
 const msalConfig: MSAL.Configuration = {
 	auth: {
-		clientId: import.meta.env.VITE_AZURE_CLIENT_ID,
+		// I would normally save this in an environment variable, the Github Pages production build is having issues with reading it though
+		// This is a throw away test project so I'm ok exposing this ID. It is unique to the project
+		clientId: '625af569-3c39-4e98-a97c-3a60973c09a6',
+		// The environment variable version of setting clientId
+		// clientId: import.meta.env.VITE_AZURE_CLIENT_ID,
 		redirectUri: baseUri,
 		// I'm using this because I'm testing with a personal microsoft account
 		authority: 'https://login.microsoftonline.com/consumers',
