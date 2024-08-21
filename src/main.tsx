5782cd6e4e311766d072b2acaf1fc453e331d023
Daniel Tonon <dan503.npm@gmail.com>
2024-08-21 12:23:40 +1000

    styles file rename

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './routes/Router.tsx'
 import { AuthContextProvider } from './components/AuthContextProvider.tsx'
